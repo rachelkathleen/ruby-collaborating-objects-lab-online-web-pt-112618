bcc388bd92a0e7322223a18eda4f6f8247ec05af
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-16 18:53:58 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,9 @@ class MP3Importer
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").map{|f| binding.pry f.gsub("#{path}/", "")}
+    @files ||= Dir.glob("#{path}/*.mp3")
+    binding.pry
+    #.map{|f| binding.pry f.gsub("#{path}/", "")}
     #Dir.entries(@path).select {|filename| filename.include?("mp3")}
   end
 
