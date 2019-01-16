68c6ccfd93689bc81de32a6a06fb6c10cfcb1d11
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-16 18:56:27 +0000

    Automatically backed up by Learn

--- spec/mp3_importer_spec.rb
+++ spec/mp3_importer_spec.rb
@@ -23,7 +23,7 @@ describe "MP3Importer" do
       test_music_path = "./spec/fixtures/mp3s"
       music_importer = MP3Importer.new(test_music_path)
 
-      expect(music_importer.files).to include("Action Bronson - Larry Csonka - indie.mp3")
+      expect(music_importer.files).to include("Action Bronson - Larry Csonka - indie")
       expect(music_importer.files).to include("Real Estate - Green Aisles - country.mp3")
       expect(music_importer.files).to include("Real Estate - It's Real - hip-hop.mp3")
       expect(music_importer.files).to include("Thundercat - For Love I Come - dance.mp3")
